8d0bc82f29b40b4542e21ba8d9c0a474b6635c67
Rick-torrellas <ricardo.torrejas@gmail.com>
2026-02-25 09:42:33 -0400

    style: cambiar el nombre del modulo, por que el otro ya estaba tomado

---
+++ src/core_log_engine/__init__.py
@@ -0,0 +1,5 @@
+from core_log_engine.adapter import AppLogger
+from core_log_engine.port import Logger
+from core_log_engine.util import SpecificLevelFilter
+
+__all__ = ["AppLogger", "Logger", "SpecificLevelFilter"]
--- src/core_logger/__init__.py
+++
@@ -1,5 +0,0 @@
-from core_logger.adapter import AppLogger
-from core_logger.port import Logger
-from core_logger.util import SpecificLevelFilter
-
-__all__ = ["AppLogger", "Logger", "SpecificLevelFilter"]
